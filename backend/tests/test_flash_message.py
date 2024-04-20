742678054058263096db4b30d3f0f31e63636a84
Hugo Mortreux <70602545+hxjo@users.noreply.github.com>
2024-04-20 16:27:38 +0200

    test: test_flash_message

--- backend/tests/test_flash_message.py
+++ backend/tests/test_flash_message.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Depends
-from typing import Annotated, TypedDict, cast
+from typing import Annotated, cast
 
 from starlette.middleware.sessions import SessionMiddleware
 from starlette.testclient import TestClient
@@ -17,7 +17,7 @@ InertiaDep = Annotated[
 ]
 
 
-PROPS= {
+PROPS = {
     "message": "hello from index",
     "messages": [{"message": "hello from flash message", "category": "info"}]
 }
@@ -37,6 +37,20 @@ async def index(inertia: InertiaDep) -> InertiaResponse:
     })
 
 
+@app.get("/redirect", response_model=None, dependencies=[Depends(dependency)])
+async def redirect_page(inertia: InertiaDep) -> InertiaResponse:
+    return await inertia.render(COMPONENT, {
+        "message": PROPS.get("message"),
+    })
+
+
+@app.get("/other-page", response_model=None)
+async def other_page(inertia: InertiaDep) -> InertiaResponse:
+    return await inertia.render(COMPONENT, {
+        "message": PROPS.get("message"),
+    })
+
+
 def test_flash_message_is_included_from_dependency() -> None:
     with TestClient(app) as client:
         response = client.get("/", headers={"X-Inertia": "true"})
@@ -46,6 +60,38 @@ def test_flash_message_is_included_from_dependency() -> None:
             "component": COMPONENT,
             "props": PROPS,
             "url": f"{client.base_url}/",
+            "version": "1.0",
+        }
+
+
+def test_flash_message_is_not_included_on_second_request() -> None:
+    with TestClient(app) as client:
+        client.get("/", headers={"X-Inertia": "true"})
+        response = client.get("/other-page", headers={"X-Inertia": "true"})
+        assert response.status_code == 200
+        assert response.headers.get('content-type').split(';')[0] == 'application/json'
+        assert response.json() == {
+            "component": COMPONENT,
+            "props": {
+                "message": PROPS.get("message"),
+                "messages": [],
+            },
+            "url": f"{client.base_url}/other-page",
+            "version": "1.0",
+        }
+
+
+def test_flash_message_is_persisted_on_redirect() -> None:
+    with TestClient(app) as client:
+        headers = {"X-Inertia": "true"}
+        response = client.get("/redirect", headers=headers)
+
+        assert response.status_code == 200
+        assert response.headers.get('content-type').split(';')[0] == 'application/json'
+        assert response.json() == {
+            "component": COMPONENT,
+            "props": PROPS,
+            "url": f"{client.base_url}/redirect",
             "version": "1.0",
         }
 
