e5206b1d80f624261509834eb86a1387925f9379
Hugo Mortreux <70602545+hxjo@users.noreply.github.com>
2024-04-20 17:17:48 +0200

    test ssr

--- backend/tests/utils.py
+++ backend/tests/utils.py
@@ -10,9 +10,11 @@ def get_stripped_html(
         url: str,
         script_asset_url: str = "http://localhost:5173/src/main.js",
         css_asset_url: str = "",
-        additional_head_content: str = ""
+        additional_head_content: str = "",
+        body_content: str = ""
 ) -> str:
     css_link = f"<link rel=\"stylesheet\" href=\"{css_asset_url}\">" if css_asset_url else ""
+    body_content = body_content or f"<div id='app' data-page='{{\"component\": \"{component_name}\", \"props\": {json.dumps(props)}, \"url\": \"{url}\", \"version\": \"1.0\"}}'></div>"
     return Template("""
                    <!DOCTYPE html>
                    <html>
@@ -23,14 +25,12 @@ def get_stripped_html(
                             $css_link
                         </head>
                         <body>
-                            <div id='app' data-page='{"component": "$component_name", "props": $props, "url": "$url", "version": "1.0"}'></div>
+                            $body_content
                             <script type="module" src="$script_asset_url"></script>
                        </body>
                    </html>
             """).substitute(
-        component_name=component_name,
-        props=json.dumps(props),
-        url=url,
+        body_content=body_content,
         head=additional_head_content,
         css_link=css_link,
         script_asset_url=script_asset_url
