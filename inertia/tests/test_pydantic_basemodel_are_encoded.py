b6e218660c321864747c2c4e71260c140fe23b92
hxjo <70602545+hxjo@users.noreply.github.com>
2024-05-31 14:52:09 +0200

    Fix: ensure Ppydantic models are properly json-ified (#19)
    # Context
    
    When building props, Pydantic models were converted to dict using
    Pydantic's `model_dump` method, which converts the model to a dict.
    When an Inertia call was made, a simple `JSONResponse` (FastAPI's one)
    was returned, with the props.
    However, JSONResponse does not handle some fields, such as `datetime`,
    and would require either:
    - Handling those prior to passing the models to the `render` method of
    inertia
    - Overriding the `render` method of inertia in order to use a custom
    `JSONResponse` which would handle those cases
    
    This is very unoptimal.
    
    # Solution
    - Instead of using Pydantic's `model_dump` method, use its
    `model_dump_json` method which handles much more types, and use
    `json.loads` to convert it back to a dict.
    
    - Expose a `_render_json` method so it can be easily overridden, without
    having to override the whole `render` method.

--- inertia/tests/test_pydantic_basemodel_are_encoded.py
+++ inertia/tests/test_pydantic_basemodel_are_encoded.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from fastapi import FastAPI, Depends
 from typing import Annotated, cast
 from pydantic import BaseModel
@@ -16,13 +17,17 @@ InertiaDep = Annotated[Inertia, Depends(inertia_dependency_factory(InertiaConfig
 class Person(BaseModel):
     name: str
     age: int
+    created_at: datetime
 
 
 PROPS = {
+    "person": {"name": "John Doe", "age": 42, "created_at": datetime.now()},
+}
+EXPECTED_PROPS = {
     "person": {
-        "name": "John Doe",
-        "age": 42,
-    },
+        **PROPS["person"],
+        "created_at": cast(datetime, PROPS["person"]["created_at"]).isoformat(),
+    }
 }
 
 COMPONENT = "IndexPage"
@@ -32,8 +37,16 @@ COMPONENT = "IndexPage"
 async def index(inertia: InertiaDep) -> InertiaResponse:
     name = PROPS["person"]["name"]
     age = PROPS["person"]["age"]
+    created_at = PROPS["person"]["created_at"]
     return await inertia.render(
-        COMPONENT, {"person": Person(name=cast(str, name), age=cast(int, age))}
+        COMPONENT,
+        {
+            "person": Person(
+                name=cast(str, name),
+                age=cast(int, age),
+                created_at=cast(datetime, created_at),
+            )
+        },
     )
 
 
@@ -44,7 +57,7 @@ def test_pydantic_basemodel_are_encoded_on_json_response() -> None:
         assert response.headers.get("content-type").split(";")[0] == "application/json"
         assert response.json() == {
             "component": COMPONENT,
-            "props": PROPS,
+            "props": EXPECTED_PROPS,
             "url": f"{client.base_url}/",
             "version": "1.0",
         }
@@ -57,5 +70,5 @@ def test_pydantic_basemodel_are_encoded_on_html_response() -> None:
         assert response.headers.get("content-type").split(";")[0] == "text/html"
         expected_url = str(client.base_url) + "/"
         assert response.text.strip() == get_stripped_html(
-            component_name=COMPONENT, props=PROPS, url=expected_url
+            component_name=COMPONENT, props=EXPECTED_PROPS, url=expected_url
         )
