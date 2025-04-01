1671f7addd7ad35753037652086cb53ec15e74b8
Hugo Mortreux <70602545+hxjo@users.noreply.github.com>
2025-04-01 20:23:12 +0200

    format

--- inertia/tests/test_special_chars_in_prop_values_are_safely_escaped.py
+++ inertia/tests/test_special_chars_in_prop_values_are_safely_escaped.py
@@ -14,34 +14,44 @@ InertiaDep = Annotated[
 ]
 
 # Include a property with special characters.
-PROPS = {
-    "special": "<>&'"
-}
+PROPS = {"special": "<>&'"}
 
 EXPECTED_PROPS = {**PROPS}
 COMPONENT = "IndexPage"
+
 
 @app.get("/", response_model=None)
 async def index(inertia: InertiaDep) -> InertiaResponse:
     return await inertia.render(COMPONENT, PROPS)
 
+
 def test_special_chars_are_escaped() -> None:
     with TestClient(app) as client:
         response = client.get("/")
-        
+
         assert response.status_code == 200
         assert response.headers.get("content-type").split(";")[0] == "text/html"
 
         html_output = response.text
 
-        match = re.search(r'<div\s+id=["\']app["\'][^>]*\sdata-page=(["\'])(.*?)\1', html_output)
+        match = re.search(
+            r'<div\s+id=["\']app["\'][^>]*\sdata-page=(["\'])(.*?)\1', html_output
+        )
         assert match, "Could not find the div with id 'app' and a data-page attribute."
         data_page_value = match.group(2)
 
-        assert "<" not in data_page_value, "Expected '<' to be escaped as \\u003c in data-page."
-        assert ">" not in data_page_value, "Expected '>' to be escaped as \\u003e in data-page."
-        assert "&" not in data_page_value, "Expected '&' to be escaped as \\u0026 in data-page."
-        assert "'" not in data_page_value, "Expected '<' to be escaped as \\u0027 in data-page."
+        assert (
+            "<" not in data_page_value
+        ), "Expected '<' to be escaped as \\u003c in data-page."
+        assert (
+            ">" not in data_page_value
+        ), "Expected '>' to be escaped as \\u003e in data-page."
+        assert (
+            "&" not in data_page_value
+        ), "Expected '&' to be escaped as \\u0026 in data-page."
+        assert (
+            "'" not in data_page_value
+        ), "Expected '<' to be escaped as \\u0027 in data-page."
 
         expected_url = str(client.base_url) + "/"
         assert_response_content(
@@ -50,4 +60,3 @@ def test_special_chars_are_escaped() -> None:
             expected_props=EXPECTED_PROPS,
             expected_url=expected_url,
         )
-
